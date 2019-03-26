1ab0b26d0214fde570768b060ca07719a8a92c17
iamniteeshmishra@gmail.com <nitish219@gmail.com>
2019-03-26 09:29:39 +0530

    Added Header and Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserLoginService } from './app.service';
+import axios from 'axios'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientSide';
+
+  constructor(private service: UserLoginService) {
+
+  }
+
+  ngOnInit() {
+    this.service.getUser().then((res) => {
+      this.title = res.data.username;
+    })
+
+    this.service.sendUser().then((res) => {
+      console.log(res.data)
+    })
+  }
+
+  sendUser() {
+    console.log('Hello');
+    var requset = axios.post('http://localhost:3000/login', { user: "Mishi" });
+    console.log(requset)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginService } from './app.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainBodyComponent } from './main-body/main-body.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserLoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
